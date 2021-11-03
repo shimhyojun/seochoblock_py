74ddd74326642398ef518d3287b70489881aa069
shimhyojun <83755070+shimhyojun@users.noreply.github.com>
2021-11-03 21:58:52 +0900

    modified by shim

--- cryptomarkets/upbit-api/openapi.py
+++ cryptomarkets/upbit-api/openapi.py
@@ -2,10 +2,11 @@
 import ccxt
 import pprint
 
-upbit = ccxt.upbit()
-btc = upbit.fetch_ticker("BTC/KRW")
+while True:
+    upbit = ccxt.upbit()
+    btc = upbit.fetch_ticker("BTC/KRW")
 
-# 뽑을 수 있는 목록 조회
-# pprint.pprint(btc)
+    # 뽑을 수 있는 목록 조회
+    pprint.pprint(btc)
 
-print("현재가",btc['last'])
+    print("현재가",btc['last'])
--- db_module.py
+++ db_module.py
@@ -30,3 +30,33 @@ class key_DB:
         
     def commit(self):
         self.db.commit()
+
+class upbit_BTC:
+    def __init__(self):
+        self.db = pymysql.connect(
+            host='skuser35-mydb.cw2or7iuke4y.us-west-1.rds.amazonaws.com',
+            user='root',
+            password='test1357',
+            db='mydb',
+            port=3306,
+            charset='utf8'
+          
+        )
+
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+        
+    def commit(self):
+        self.db.commit()
